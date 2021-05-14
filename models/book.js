9bee6556142a28b335cae63101797400dc9b878f
Bella Santiago <bellasantiago.adl@gmail.com>
2021-05-14 14:10:47 +0930

    models

--- models/book.js
+++ models/book.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const booksSchema = new Schema({
-    
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    link: { type: String },
+    image: { type: String },
+    description: { type: String }
 });
 
 const Books = mongoose.model("Books", booksSchema);
