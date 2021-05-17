b5596b4b1f532084f4e8040b936799ac9dc11336
Bella Santiago <bellasantiago.adl@gmail.com>
2021-05-17 20:15:37 +0930

    Book Search functional

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,59 +1,33 @@
 import React, { Component } from 'react';
-import API from "../utils/API";;
 
 class Results extends Component {
-    state = {
-        myBooks: [],
-        value: ""
-    };
-
-    componentDidMount() {
-        API.myBooks()
-            .then(myBooks => this.setState({ myBooks: myBooks }))
-            .catch(err => console.error(err));
-    }
-
-    bookData = bookData => {
-        return {
-            _id: bookData.id,
-            image: bookData.volumeInfo.imageLinks.thumbnail,
-            title: bookData.volumeInfo.title,
-            authors: bookData.volumeInfo.authors,
-            description: bookData.volumeInfo.description
-        }
-    }
-
-    search = bookSearch => {
-        API.getBooks(bookSearch)
-            .then(res => this.setState({ myBooks: res.data.items.map(bookData => this.bookData(bookData)) }))
-            .catch(err => console.error(err));
-    };
-
-    handleFormSubmit = event => {
-        event.preventDefault();
-        this.search(this.state.search);
-    };
 
     render() {
         return (
             <div>
-                {this.state.myBooks.map(result => (
-                    <div class="row justify-content-center no-gutters mb-5 mb-lg-0" key={result._id}>
+                {this.props.myBooks.map(result => (
+                    <div class="row justify-content-center no-gutters" key={result._id}>
                         <div className="row">
-                            <div class="col-lg-6"><img class="img-fluid" src={result.image} alt="..." /></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-2"><img class="img-fluid" src={result.image} alt="..." /></div>
+                            <div class="col-lg-10">
                                 <div class="bg-black text-center h-100 project">
                                     <div class="d-flex h-100">
                                         <div class="project-text w-100 my-auto text-center text-lg-left">
                                             <h4 class="text-white">{result.title} by {result.authors}</h4>
                                             <p class="mb-0 text-white-50">{result.description}</p>
-                                            <hr class="d-none d-lg-block mb-0 ml-0" />
+                                            <hr class="d-none d-lg-block mb-0" />
+                                            <div>
+                                                <button className="btn badge-pill btn-primary mt-3 ml-3" > Save
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <br></br>
                     </div>
+
                 ))}
             </div>
         )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Hero from '../components/Hero';
 import Search from '../components/Search';
 import Results from '../components/Results'
+import API from "../utils/API";;
 
-function Home() {
+
+
+class Home extends Component {
+
+  state = {
+    myBooks: [],
+    search: ""
+  };
+
+  componentDidMount() {
+    this.search();
+    API.myBooks()
+      .then(myBooks =>
+        this.setState({ myBooks: myBooks }))
+      .catch(err =>
+        console.error(err));
+  }
+
+  bookData = bookData => {
+    return {
+      _id: bookData.id,
+      image: bookData.volumeInfo.imageLinks.thumbnail,
+      title: bookData.volumeInfo.title,
+      authors: bookData.volumeInfo.authors,
+      description: bookData.volumeInfo.description
+    }
+  }
+
+  search = bookSearch => {
+    API.getBooks(bookSearch)
+      .then(res =>
+        this.setState({
+          myBooks: res.data.items.map(bookData =>
+            this.bookData(bookData))
+        }))
+      .catch(err =>
+        console.error(err));
+  };
+
+   handleFormSubmit = event => {
+    event.preventDefault();
+    this.search(this.state.search);
+    this.setState({search: ""})
+  };
+
+   handleInputChange = event => {
+    event.preventDefault();
+    console.log(event);
+    this.setState({search: event.target.value})
+  }
+
+  render() {
     return (
-        <div>
-          <Hero />
-          <Search 
-          title="Search"
-          id="Search"
+      <div>
+        <Hero />
+          <Search
+            title="Search"
+            id="Search"
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            search={this.state.search}
           />
           <br></br>
           <br></br>
-          <Results />
-        </div>
+          <Results 
+            myBooks={this.state.myBooks}
+          />
+      </div>
     )
+  }
 }
 
 export default Home
